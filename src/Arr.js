e9f3c4089510d9e04b4f805b3beafe77e6688ead
Krunal01 <parmarkrunal09101@gmail.com>
2022-10-13 20:15:34 +0530

    Nested list or array with Nested tables

--- src/Arr.js
+++ src/Arr.js
@@ -1,9 +1,21 @@
 import React from "react";
 function Arr() {
   const students = [
-    { name: "Rahul", id: 10 },
-    { name: "Kamlesh", id: 20 },
-    { name: "Dipak", id: 10 },
+    {
+      name: "Rahul",
+      id: 10,
+      fullname: [{ fname: "rk" }, { mname: "kp" }, { surname: "patel" }],
+    },
+    {
+      name: "Kamlesh",
+      id: 20,
+      fullname: [{ fname: "rk" }, { mname: "kp" }, { surname: "patel" }],
+    },
+    {
+      name: "Dipak",
+      id: 10,
+      fullname: [{ fname: "rk" }, { mname: "kp" }, { surname: "patel" }],
+    },
   ];
   //   const emp = [0, 1, 2, 3];
   //   students.map((e) => {
@@ -18,15 +30,29 @@ function Arr() {
           <tr>
             <th> Name </th>
             <th> ID </th>
+            <th> FullName </th>
           </tr>
-          {students.map((e, i) =>
-            e.id === 10 ? (
-              <tr key={i}>
-                <td>{e.name}</td>
-                <td>{e.id}</td>
-              </tr>
-            ) : null
-          )}
+          {students.map((e, i) => (
+            <tr key={i}>
+              <td>{e.name}</td>
+              <td>{e.id}</td>
+              <td>
+                <table>
+                  <tbody>
+                    {e.fullname.map((s) => (
+                      <tr key={i}>
+                        <td>
+                          {s.fname}
+                          {s.mname}
+                          {s.surname}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
