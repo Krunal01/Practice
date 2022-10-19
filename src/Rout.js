aed1c7ec4d245c77f3de338915908ecedea918b7
Krunal01 <parmarkrunal09101@gmail.com>
2022-10-19 20:07:58 +0530

    Page 404

--- src/Rout.js
+++ src/Rout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import SubRoute from "./SubRoute";
 import NestedRout from "./NestedRout";
+import Page404 from "./Page404";
 
 function Rout() {
   return (
@@ -14,6 +15,7 @@ function Rout() {
         <Routes>
           <Route path="/" element={<SubRoute />} />
           <Route path="/NestedRout" element={<NestedRout />} />
+          <Route path="/*" element={<Page404 />} />
         </Routes>
       </BrowserRouter>
     </div>
